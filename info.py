b393751cdfc74aa14726e0023b0c288c9b78b3bd
priest1966 <53721478+priest1966@users.noreply.github.com>
2024-07-27 22:10:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movieverse_discussion_2')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movieverse_2')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtoverify_97/3')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movieverse_disscussion_2') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'movieverse_discussion_2') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
